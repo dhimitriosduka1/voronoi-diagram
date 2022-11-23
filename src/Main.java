33b11fdbcd23f5b63b0e7cf70b10cd39f6990d74
dhimitriosduka <dhimitrios.duka1@gmail.com>
2022-11-23 23:53:56 +0100

    Implemented Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import utils.ImageUtils;
+import entity.Site;
+import utils.SiteUtils;
+import entity.Voronoi;
+import ppm.PPMImageWriter;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    final static int WIDTH = 800;
+    final static int HEIGHT = 600;
+    final static int RADIUS = 5;
+    final static int SITE_AMOUNT = 10;
+
+    final static String PATH = "output.ppm";
+
+    public static void main(String[] args) throws IOException {
+
+        Color[][] image = ImageUtils.init(HEIGHT, WIDTH);
+
+        List<Site> sites = SiteUtils.initRandom(SITE_AMOUNT, RADIUS, image);
+
+        Voronoi.render(sites, image);
+
+        SiteUtils.fill(sites, image);
+
+        PPMImageWriter.saveImageAsPPM(PATH, image);
+
     }
 }
