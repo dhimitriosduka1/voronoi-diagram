9a6a62e24dc4ff5de3bab35b28379eb3330dce8a
dhimitriosduka <dhimitrios.duka1@gmail.com>
2022-11-24 00:01:18 +0100

    Implemented manhattan distance

--- src/utils/MathUtils.java
+++ src/utils/MathUtils.java
@@ -2,10 +2,16 @@ package utils;
 
 public class MathUtils {
 
-    public static int sqrt(int x0, int y0, int x1, int y1) {
+    public static int euclideanDistance(int x0, int y0, int x1, int y1) {
         int dx = x1 - x0;
         int dy = y1 - y0;
-        return dx * dx + dy * dy;
+        return (int) Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public static int manhattanDistance(int x0, int y0, int x1, int y1) {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        return Math.abs(dx) + Math.abs(dy);
     }
 
 }
