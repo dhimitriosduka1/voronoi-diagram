236002504d1c06ecfb9352b1fd0aeff706c5eb3f
dhimitriosduka <dhimitrios.duka1@gmail.com>
2022-11-23 23:53:18 +0100

    First implementation of Voronoi class

---
+++ src/entity/Voronoi.java
@@ -0,0 +1,40 @@
+package entity;
+
+import utils.MathUtils;
+
+import java.awt.*;
+import java.util.List;
+
+public class Voronoi {
+
+    private static final List<Color> pallet = List.of(
+            Color.ORANGE,
+            Color.BLUE,
+            Color.YELLOW,
+            Color.GREEN,
+            Color.RED,
+            Color.DARK_GRAY,
+            Color.MAGENTA,
+            Color.CYAN
+    );
+
+    public static void render(List<Site> sites, Color[][] image) {
+        for (int i = 0; i < image[0].length; i++) {
+            for (int j = 0; j < image.length; j++) {
+                int closestSiteIndex = 0;
+                for (int k = 1; k < sites.size(); k++) {
+                    Site closestSite = sites.get(closestSiteIndex);
+                    Site candidateSite = sites.get(k);
+
+                    int distanceOne = MathUtils.sqrt(closestSite.getX(), closestSite.getY(), i, j);
+                    int distanceTwo = MathUtils.sqrt(candidateSite.getX(), candidateSite.getY(), i, j);
+
+                    if (distanceTwo < distanceOne) {
+                        closestSiteIndex = k;
+                    }
+                }
+                image[j][i] = pallet.get(closestSiteIndex % pallet.size());
+            }
+        }
+    }
+}
